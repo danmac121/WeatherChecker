1982ffb4478ed792967c6f95e68e4f65b17e9848
danmac121 <danmacdonald121@gmail.com>
2023-08-04 17:22:10 -0400

    Added the event listener on the search button, added a variable in that event listener that is equal to the value of the text input. Placed that variable in the geo fetch, it now returns the coordinates of the text put into the input box and logs coords to console

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,28 +1,28 @@
+// set the city to the text content of the user input on submit click
+// event listener on div so we can listen to child 
+// make variable for city to be put into the first api fetch
 
 
+let search = document.getElementById("search")
+// let userText = document.getElementById("userText").value;
+
+search.addEventListener("click" , function(event){
+
+if (event.target.matches("button")) {
+    let userText = document.getElementById("userText").value;
 
 
-
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=61a660a594379343aaaaa4c93e1f2fc8", {
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userText}&limit=5&appid=61a660a594379343aaaaa4c93e1f2fc8`, {
  
 })
   .then(response => response.json()) 
-  .then(citiesFound =>{
-    let firstCity = citiesFound[0]
+  .then(data =>{
+    let firstCity = data[0]
+    console.log(data)
     console.log(firstCity.lat)
     console.log(firstCity.lon)
     return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=61a660a594379343aaaaa4c93e1f2fc8`)
   })
-  
-
-
-
-
-
-
-
-
-
 
   .then(function (response) {
     return response.json();
@@ -30,9 +30,9 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=61a66
   .then(function (data) {
     console.log(data);
   });
-
-
-
+  
+}
+})
 
 
 
